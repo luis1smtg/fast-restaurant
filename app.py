c637456f6ecc4837b8a4632a7c0e366327fade0b
Luis Maldonado <luis@1smtg.com>
2022-06-06 10:58:47 -0700

    test

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
+from escpos.printer import Network
+
+kitchen = Network("192.168.1.100") #Printer IP Address
+kitchen.text("Hello World\n")
+kitchen.barcode('1324354657687', 'EAN13', 64, 2, '', '')
+kitchen.cut()
+
 app = Flask(__name__)
 
 @app.route("/")
